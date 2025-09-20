7ea581f7c2d08d19b34f86d0689fb6499b34629f
LautyAS <lautaroanzstr@gmail.com>
2025-09-20 19:41:02 -0300

    Ricing

--- 07-ricing.sh
+++ 07-ricing.sh
@@ -9,44 +9,16 @@ USER=$(ls /mnt/home | head -n 1)
 USER_HOME="/mnt/home/$USER"
 
 # Directorios locales de tus configs y rice (desde donde ejecutás el script)
-CONFIGS_DIR="./configs"
-RICE_DIR="./rice"
-
-# Comprobar existencia de carpetas
-if [[ ! -d "$CONFIGS_DIR" ]]; then
-    echo "No se encontró la carpeta configs."
-    exit 1
-fi
-
-if [[ ! -d "$RICE_DIR" ]]; then
-    echo "No se encontró la carpeta rice."
-    exit 1
-fi
+CONFIGS_DIR="./configs/.config"
 
 # Crear .config en el sistema instalado
 mkdir -p "$USER_HOME/.config"
+mkdir -p /mnt/etc/
 
 echo "Copiando dotfiles desde configs..."
 cp -r "$CONFIGS_DIR/"* "$USER_HOME/.config/"
+cp -r "./configs/etc"* "/mnt/etc"
 echo "Copiadas configs principales."
-
-echo "Aplicando rice adicional..."
-for d in "$RICE_DIR/"*; do
-    if [[ -d "$d" ]]; then
-        cp -r "$d" "$USER_HOME/.config/"
-        echo "Copiado $(basename "$d") a .config"
-    fi
-done
-
-# Aplicar wallpaper si existe
-WALLPAPER_SRC="$CONFIGS_DIR/wallpaper.jpg"
-if [[ -f "$WALLPAPER_SRC" ]]; then
-    mkdir -p "$USER_HOME/wallpapers"
-    cp "$WALLPAPER_SRC" "$USER_HOME/wallpapers/"
-    echo "Wallpaper copiado. Se aplicará al iniciar sesión."
-else
-    echo "No se encontró wallpaper.jpg en configs."
-fi
 
 # Ajustar permisos dentro del chroot (ahí sí existe el usuario)
 arch-chroot /mnt /bin/bash <<EOF
