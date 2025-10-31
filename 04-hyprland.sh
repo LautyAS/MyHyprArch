54c5afe69fba8c9a8dd69ebf8c38520e267fbbea
LautyAS <106852696+LautyAS@users.noreply.github.com>
2025-10-31 05:12:37 -0300

    Posibly working AUR

--- 04-hyprland.sh
+++ 04-hyprland.sh
@@ -25,29 +25,45 @@ fi
 echo "📦 Actualizando sistema..."
 pacman -Syu --noconfirm git base-devel
 
-#echo "💻 Instalando paru (AUR helper)..."
-#cd /tmp
-#git clone https://aur.archlinux.org/paru-bin.git
-#cd paru-bin
-#makepkg -si --noconfirm
-#cd ..
-#rm -rf paru-bin
+echo "💻 Instalando paru (AUR helper)..."
+su - "$USERNAME" -c "
+cd /home/$USERNAME && \
+git clone https://aur.archlinux.org/paru-bin.git && \
+cd paru-bin && \
+makepkg -f --noconfirm 
+"
+
+cd /home/$USERNAME/paru-bin
+for pkgfile in paru-bin*.pkg.tar.zst; do
+    pacman -U --noconfirm "$pkgfile"
+done
+cd /home/$USERHOME
+rm -rf paru-bin
+
+
+echo "Dando permisos de ejecución de paru sin contraseña..."
+
+echo "$USERNAME ALL=(ALL) NOPASSWD: /usr/bin/paru, /usr/bin/pacman" | sudo tee /etc/sudoers.d/paru > /dev/null
+sudo chmod 440 /etc/sudoers.d/paru
 
 echo "🎨 Instalando Hyprland, utilidades y floorp..."
-pacman -S --noconfirm \
-    hyprland hyprpaper kitty waybar wofi ly \
-    pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber lib32-pipewire pavucontrol \
-    lib32-mesa mesa-utils vulkan-tools \
-    fcitx5-im fcitx5 fcitx5-configtool fcitx5-gtk fcitx5-qt fcitx5-mozc \
-    polkit polkit-gnome \
-    nmcli nm-applet\
-    htop fastfetch \
-    bash-completion \
-    xdg-desktop-portal xdg-desktop-portal-wlr \
-    spotify-launcher steam \
-    firefox \
+
+packages = (
+    hyprland hyprpaper kitty waybar wofi ly
+    pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber lib32-pipewire pavucontrol
+    lib32-mesa mesa-utils vulkan-tools
+    fcitx5-im fcitx5 fcitx5-configtool fcitx5-gtk fcitx5-qt fcitx5-mozc
+    polkit polkit-gnome
+    nmcli nm-applet
+    htop fastfetch
+    bash-completion
+    xdg-desktop-portal xdg-desktop-portal-wlr
+    spotify-launcher steam
+    floorp-bin
     noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-nerd-fonts-symbols ttf-noto-nerd ttf-firacode-nerd ttf-sourcecodepro-nerd ttf-jetbrains-mono ttf-roboto
-    
+    )
+
+su - "$USERNAME" -c "paru -S --noconfirm ${packages[@]}"
 
 echo "🔌 Habilitando servicios necesarios..."
 systemctl enable NetworkManager
